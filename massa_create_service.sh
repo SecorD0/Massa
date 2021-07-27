b2fdd9b16319c475bfc0533a7016beca8b8e168e
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-07-28 00:07:20 +0300

    Creates and starts service file with node

--- massa_create_service.sh
+++ massa_create_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -s https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh | bash
 sudo tee <<EOF >/dev/null /etc/systemd/system/massad.service
 [Unit]
 Description=Massa Node
@@ -18,3 +19,4 @@ EOF
 sudo systemctl enable massad
 sudo systemctl daemon-reload
 sudo systemctl start massad
+echo -e '\e[40m\e[92mDone!\e[0m'
