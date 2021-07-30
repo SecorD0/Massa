783fb775283deddbc84b4a70c2c51b2678a62609
SecorD0 <vovan6573@gmail.com>
2021-07-30 22:45:20 +0300

    Node auto-installation

--- massa.sh
+++ massa.sh
@@ -61,7 +61,7 @@ curl -s https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh | bash
 echo -e '\nThe node was \e[40m\e[92mstarted\e[0m, the client was \e[40m\e[92mcompiled\e[0m, the wallet was \e[40m\e[92mcreated\e[0m.\n'
 echo -e 'Remember to save this files:'
 echo -e "\e[40m\e[92m/root/massa/massa-node/config/node_privkey.key\e[0m"
-echo -e "\e[40m\e[92m/root/massa/massa-client/wallet.dat\e[0m"
+echo -e "\e[40m\e[92m/root/massa/massa-client/wallet.dat\e[0m\n\n"
 echo -e '\tv \e[40m\e[92mUseful commands\e[0m v\n'
 echo -e 'To start a client for blockchain interaction: \e[40m\e[92mcd $HOME/massa/massa-client/; cargo run --release; cd\e[0m'
 echo -e 'To start a client for wallet interaction: \e[40m\e[92mcd $HOME/massa/massa-client/; cargo run -- --wallet wallet.dat; cd\e[0m'
