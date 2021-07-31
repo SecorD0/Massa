d5ca1e55236f21e5b8379e9919cc34b22c1ea1cd
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-07-31 23:47:36 +0300

    Opens required ports

---
+++ ports_openning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+iptables -I INPUT -p tcp --dport 31244 -j ACCEPT
+iptables -I INPUT -p tcp --dport 31245 -j ACCEPT
+IP=$(wget -qO- eth0.me)
+sed -i "/\[consensus\]/a routable_ip=\"$IP\"" "$HOME/massa/massa-node/config/config.toml"
+systemctl restart massad
