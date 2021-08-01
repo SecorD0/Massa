8e2d0b1b7ac405d648c5a7184f199011f996d377
SecorD0 <vovan6573@gmail.com>
2021-08-01 21:45:59 +0300

    Opens required ports

--- ports_openning.sh
+++ ports_openning.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-iptables -I INPUT -p tcp --dport 31244 -j ACCEPT
-iptables -I INPUT -p tcp --dport 31245 -j ACCEPT
-IP=$(wget -qO- eth0.me)
-sed -i "/\[network\]/a routable_ip=\"$IP\"" "$HOME/massa/massa-node/config/config.toml"
+systemctl stop massad
+if sudo ufw status | grep "Status: active"; then
+	sudo ufw allow 31244
+	sudo ufw allow 31245
+else
+	sudo iptables -I INPUT -p tcp --dport 31244 -j ACCEPT
+	sudo iptables -I INPUT -p tcp --dport 31245 -j ACCEPT
+	sudo apt-get -y install iptables-persistent
+	sudo netfilter-persistent save
+fi
+if ! sudo grep "routable_ip" "$HOME/massa/massa-node/config/config.toml"; then
+	IP=$(wget -qO- eth0.me)
+	sed -i "/\[network\]/a routable_ip=\"$IP\"" "$HOME/massa/massa-node/config/config.toml"
+fi
 systemctl restart massad
