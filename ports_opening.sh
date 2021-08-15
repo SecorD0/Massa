47f1fef531bbcec34ca6c926ef4c77ca04fbaa81
SecorD0 <vovan6573@gmail.com>
2021-08-15 23:52:17 +0300

    Opens required ports

--- ports_opening.sh
+++ ports_opening.sh
@@ -8,11 +8,10 @@ else
 	sudo iptables -I INPUT -p tcp --dport 31245 -j ACCEPT
 	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 	echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-	sudo apt -y install iptables-persistent
+	sudo apt install iptables-persistent -y
 	sudo netfilter-persistent save
 fi
 if ! sudo grep -q "routable_ip" "$HOME/massa/massa-node/config/config.toml"; then
-	IP=$(wget -qO- eth0.me)
-	sed -i "/\[network\]/a routable_ip=\"$IP\"" "$HOME/massa/massa-node/config/config.toml"
+	sed -i "/\[network\]/a routable_ip=\"$(wget -qO- eth0.me)\"" "$HOME/massa/massa-node/config/config.toml"
 fi
 systemctl restart massad
