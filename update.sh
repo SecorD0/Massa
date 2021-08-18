fc5d86ad5955b2ba0d62f1998f202a5e27904ff1
SecorD0 <vovan6573@gmail.com>
2021-08-18 12:02:25 +0300

    Updates the node

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo apt update
-sudo apt install curl -y
-curl -s https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh | bash
+sudo apt install wget -y
+. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 sudo systemctl stop massad
 mkdir $HOME/massa_buckup
 sudo mv $HOME/massa/massa-client/wallet.dat $HOME/massa_buckup/wallet.dat
 sudo mv $HOME/massa/massa-node/config/node_privkey.key $HOME/massa_buckup/node_privkey.key
 sudo rm -rf $HOME/massa
 sudo apt upgrade -y
-sudo apt install pkg-config curl git build-essential libssl-dev -y
+sudo apt install pkg-config git build-essential libssl-dev -y
 cd
 git clone --branch testnet https://gitlab.com/massalabs/massa.git
 echo -e '\e[40m\e[92mNode installation...\e[0m'
@@ -23,15 +23,14 @@ cd $HOME/massa/massa-client/
 cargo build --release
 sudo cp $HOME/massa_buckup/wallet.dat $HOME/massa/massa-client/wallet.dat
 massa_wallet_address=$(cargo run --release wallet_info | jq -r ".balances | keys[]")
-curl -s https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh | bash -s "massa_wallet_address" $massa_wallet_address
-curl -s https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh | bash -s "massa_client" "cd $HOME/massa/massa-client/; cargo run --release; cd" true
-curl -s https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh | bash -s "massa_status" "journalctl -n 100 -f -u massad" true
-. ~/.bash_profile
+. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_address" $massa_wallet_address
+. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_client" "cd \$HOME\/massa\/massa-client\/; cargo run --release; cd" true
+. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_status" "journalctl -n 100 -f -u massad" true
 cargo run --release -- buy_rolls $massa_wallet_address 20 0
 cargo run --release -- register_staking_keys $(cargo run --release wallet_info | jq -r ".wallet[0]")
 cd
 echo -e '\e[40m\e[92mDone!\e[0m'
-curl -s https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh | bash
+. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 echo -e '\nThe node was \e[40m\e[92mstarted\e[0m, the client was \e[40m\e[92mcompiled\e[0m, the wallet was \e[40m\e[92mcreated\e[0m.\n'
 echo -e 'Remember to save this files:'
 echo -e "\e[40m\e[92m/root/massa/massa-node/config/node_privkey.key\e[0m"
