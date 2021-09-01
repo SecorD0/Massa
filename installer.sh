32b2b01a996ce39954a308316a893615862de74d
SecorD0 <vovan6573@gmail.com>
2021-09-02 01:29:42 +0300

    Node auto-installation

--- installer.sh
+++ installer.sh
@@ -14,7 +14,7 @@ if [ ! -d $HOME/massa/ ]; then
 fi
 echo -e '\e[40m\e[92mNode installation...\e[0m'
 cd $HOME/massa/massa-node/
-RUST_BACKTRACE=full cargo build --release |& tee logs.txt
+RUST_BACKTRACE=full cargo build --release
 sudo tee <<EOF >/dev/null /etc/systemd/system/massad.service
 [Unit]
 Description=Massa Node
@@ -38,19 +38,23 @@ echo -e '\e[40m\e[92mDone!\e[0m'
 echo -e '\e[40m\e[92mClient installation...\e[0m'
 cd $HOME/massa/massa-client/
 cargo run --release wallet_new_privkey
-massa_wallet_address=$(cargo run --release wallet_info | jq -r ".balances | keys[]")
+massa_wallet_address=$(cargo run --release -- --cli true wallet_info | jq -r ".balances | keys[]")
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_address" $massa_wallet_address
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_client" "cd \$HOME\/massa\/massa-client\/; cargo run --release; cd" true
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_log" "journalctl -n 100 -f -u massad" true "massa_status"
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_log" "journalctl -f -n 100 -u massad" true "massa_status"
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_client" "cd \$HOME/massa/massa-client/; cargo run --release; cd" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_info" "cd \$HOME/massa/massa-client/; cargo run --release -- --cli false wallet_info 2> /dev/null; cd" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_peers" "cd \$HOME/massa/massa-client/; cargo run --release -- --cli false peers 2> /dev/null; cd" true
 cd
 echo -e '\e[40m\e[92mDone!\e[0m'
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 echo -e '\nThe node was \e[40m\e[92mstarted\e[0m, the client was \e[40m\e[92mcompiled\e[0m, the wallet was \e[40m\e[92mcreated\e[0m.\n'
 echo -e 'Remember to save this files:'
-echo -e "\e[40m\e[92m/root/massa/massa-node/config/node_privkey.key\e[0m"
-echo -e "\e[40m\e[92m/root/massa/massa-client/wallet.dat\e[0m\n\n"
+echo -e "\033[0;31m$HOME/massa/massa-node/config/node_privkey.key\e[0m"
+echo -e "\033[0;31m$HOME/massa/massa-client/wallet.dat\e[0m\n\n"
 echo -e '\tv \e[40m\e[92mUseful commands\e[0m v\n'
 echo -e 'To start a client: \e[40m\e[92mmassa_client\e[0m'
+echo -e 'To view a wallet info: \e[40m\e[92mmassa_wallet_info\e[0m'
+echo -e 'To view peers: \e[40m\e[92mmassa_peers\e[0m'
 echo -e 'To view the node status: \e[40m\e[92msystemctl status massad\e[0m'
-echo -e 'To view the node log: \e[40m\e[92mmassa_status\e[0m'
+echo -e 'To view the node log: \e[40m\e[92mmassa_log\e[0m'
 echo -e 'To restart the node: \e[40m\e[92msystemctl restart massad\e[0m\n'
