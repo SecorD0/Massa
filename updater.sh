ed0c3ddfe47a373977b9fb0428c04afb4856adee
SecorD0 <vovan6573@gmail.com>
2021-09-07 02:05:23 +0300

    Updates the node

--- updater.sh
+++ updater.sh
@@ -1,45 +1,56 @@
 #!/bin/bash
-sudo apt update
 sudo apt install wget -y
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
-sudo systemctl stop massad
+sudo apt update
+sudo apt upgrade -y
+sudo apt install wget jq unzip git build-essential pkg-config libssl-dev -y
 mkdir $HOME/massa_backup
 sudo cp $HOME/massa/massa-client/wallet.dat $HOME/massa_backup/wallet.dat
 sudo cp $HOME/massa/massa-node/config/node_privkey.key $HOME/massa_backup/node_privkey.key
-sudo apt upgrade -y
-sudo apt install curl jq pkg-config git build-essential libssl-dev -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. $HOME/.cargo/env
-rustup toolchain install nightly
-rustup default nightly
-cd $HOME/massa/
-git stash
-git checkout testnet
-git pull
 echo -e '\e[40m\e[92mNode installation...\e[0m'
-cd $HOME/massa/massa-node/
-RUST_BACKTRACE=full cargo build --release
+rm -rf $HOME/massa/
+wget -qO massa.zip https://gitlab.com/massalabs/massa/-/jobs/1566540813/artifacts/download
+unzip massa.zip
+rm -rf massa.zip
+sudo tee <<EOF >/dev/null /etc/systemd/system/massad.service
+[Unit]
+Description=Massa Node
+After=network-online.target
+
+[Service]
+User=$USER
+WorkingDirectory=$HOME/massa/massa-node
+ExecStart=$HOME/massa/massa-node/massa-node
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable massad
+sudo systemctl daemon-reload
 sudo cp $HOME/massa_backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 sed -i "s%.*# routable_ip *=.*%routable_ip=\"$(wget -qO- eth0.me)\"%" "$HOME/massa/massa-node/config/config.toml"
 sudo systemctl restart massad
-echo -e '\e[40m\e[92mDone!\e[0m'
-echo -e '\e[40m\e[92mClient installation...\e[0m'
 cd $HOME/massa/massa-client/
-cargo build --release
 sudo cp $HOME/massa_backup/wallet.dat $HOME/massa/massa-client/wallet.dat
-massa_wallet_address=$(cargo run --release -- --cli true wallet_info | jq -r ".balances | keys[]")
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_address" $massa_wallet_address
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_log" "journalctl -f -n 100 -u massad" true "massa_status"
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_client" "cd \$HOME/massa/massa-client/; cargo run --release; cd" true
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_info" "cd \$HOME/massa/massa-client/; cargo run --release -- --cli false wallet_info 2> /dev/null; cd" true
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_peers" "cd \$HOME/massa/massa-client/; cargo run --release -- --cli false peers 2> /dev/null; cd" true
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_version" "cd \$HOME/massa/massa-client/; cargo run --release -- --cli false version 2> /dev/null; cd" true
-cargo run --release -- buy_rolls $massa_wallet_address 1 0
-cargo run --release -- register_staking_keys $(cargo run --release -- --cli true wallet_info | jq -r ".wallet[0]")
+wallet_address="null"
+while [ "$wallet_address" = "null" ]
+do
+	wallet_address=$(./massa-client --cli true wallet_info | jq -r ".balances | keys[-1]")
+	continue
+done
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_address" $wallet_address
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_log" "sudo journalctl -f -n 100 -u massad" true "massa_status"
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_client" "cd \$HOME/massa/massa-client/; ./massa-client; cd" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_wallet_info" "cd \$HOME/massa/massa-client/; ./massa-client --cli false wallet_info; cd" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_peers" "cd \$HOME/massa/massa-client/; ./massa-client --cli false peers; cd" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "massa_version" "cd \$HOME/massa/massa-client/; ./massa-client --cli false version; cd" true
 cd
 echo -e '\e[40m\e[92mDone!\e[0m'
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
-echo -e '\nThe node was \e[40m\e[92mstarted\e[0m, the client was \e[40m\e[92mcompiled\e[0m.\n'
+echo -e '\nThe node was \e[40m\e[92mupdated\e[0m and \e[40m\e[92mstarted\e[0m!\n'
 echo -e 'Remember to save files in this directory:'
 echo -e "\033[0;31m$HOME/massa_backup/\e[0m\n\n"
 echo -e '\tv \e[40m\e[92mUseful commands\e[0m v\n'
