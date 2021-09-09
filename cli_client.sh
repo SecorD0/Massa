7209f4905d2ed30f83bc1167d89e6707ca7ca9bd
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-09-09 03:50:24 +0300

    Shortcut commands

--- cli_client.sh
+++ cli_client.sh
@@ -2,13 +2,13 @@
 # $1 - Action type
 # $2 - Buy for the whole balance? (true/false)
 cd $HOME/massa/massa-client/
+wallet_info=`./massa-client --cli true wallet_info`
+address=`jq -r ".balances | keys[0]" <<< $wallet_info`
 if [ "$1" = "massa_client" ]; then
 	./massa-client
 elif [ "$1" = "massa_wallet_info" ]; then
 	./massa-client --cli false wallet_info
 elif [ "$1" = "massa_buy_rolls" ]; then
-	wallet_info=`./massa-client --cli true wallet_info`
-	address=`jq -r ".balances | keys[0]" <<< $wallet_info`
 	if [ "$2" = "true" ]; then
 		balance=`printf "%d" jq -r ".balances[].final_ledger_data.balance" <<< $wallet_info`
 		roll_count=$(($balance/100))
@@ -21,7 +21,11 @@ elif [ "$1" = "massa_peers" ]; then
 elif [ "$1" = "massa_version" ]; then
 	./massa-client --cli false version
 elif [ "$1" = "massa_next_draws" ]; then
-	./massa-client next_draws `./massa-client --cli true wallet_info | jq -r '.balances | keys[0]'`
+	./massa-client next_draws $address
+elif [ "$1" = "register_staking_keys" ]; then
+	./massa-client register_staking_keys $(./massa-client --cli true wallet_info | jq -r ".wallet[0]")
+elif [ "$1" = "cmd_testnet_rewards_program" ]; then
+	./massa-client cmd_testnet_rewards_program $address `read -p $'\e[40m\e[92mВведите Discord ID:\e[0m ' R; echo $R`
 else
 	echo -e "\033[0;31mYou didn't specify an action\e[0m"
 fi
