b8db4ddeed124bb07b30e1cec7c96682c5167523
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-09-09 11:18:47 +0300

    Shortcut commands

--- cli_client.sh
+++ cli_client.sh
@@ -10,7 +10,8 @@ elif [ "$1" = "massa_wallet_info" ]; then
 	./massa-client --cli false wallet_info
 elif [ "$1" = "massa_buy_rolls" ]; then
 	if [ "$2" = "true" ]; then
-		balance=`printf "%d" jq -r ".balances[].final_ledger_data.balance" <<< $wallet_info`
+		balance_float=`jq -r ".balances[].final_ledger_data.balance" <<< $wallet_info`
+		balance=`printf "%d" $balance_float`
 		roll_count=$(($balance/100))
 	else
 		read -p $'\e[40m\e[92mВведите количество покупаемы ROLL\'ов:\e[0m ' roll_count
