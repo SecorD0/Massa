1c0bbb28b3ff66cabf2c40a15e263092fbb1fe3e
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-09-22 01:23:28 +0300

    Shortcut commands

--- cli_client.sh
+++ cli_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # $1 - Language (RU/EN)
-# $2 - Action type
+# $2 - Action
 # $3 - Buy for the whole balance? (true/false)
 # Functions
 printf_n(){ printf "$1\n" "${@:2}"; }
@@ -10,6 +10,13 @@ G="\e[40m\e[92m"
 R="\033[0;31m"
 # Texts
 if [ "$1" = "RU" ]; then
+	t_wi1="${G}Основной кошелёк${W}"
+	t_wi2="Адрес кошелька:  ${G}%s${W}"
+	t_wi3="Зарегистрирован для стейкинга: ${G}да${W}"
+	t_wi4="Зарегистрирован для стейкинга: ${R}нет${W}"
+	t_wi6="Баланс:          ${G}%.2f${W}"
+	t_wi7="ROLL'ов всего:   ${G}%d${W}"
+	t_wi8="Активные ROLL'ы: ${G}%d${W}"
 	t_br1="${R}Баланс менее 100 токенов${W}"
 	t_br2="Куплено ${G}%d${W} ROLL'ов"
 	t_br3="${G}Введите количество ROLL'ов:${W} "
@@ -20,9 +27,16 @@ if [ "$1" = "RU" ]; then
 	t_ctrp="${G}Введите Discord ID:${W} "
 	t_done="${G}Готово!${W}"
 	t_err="${R}Нет такого действия!${W}"
-# Send Pull request with new texts to add a language
+# Send Pull request with new texts to add a language - https://github.com/SecorD0/Massa/blob/main/cli_client.sh
 #elif [ "$1" = ".." ]; then
 else
+	t_wi1="${G}The main wallet${W}"
+	t_wi2="Wallet address: ${G}%s${W}"
+	t_wi3="Registered for staking: ${G}yes${W}"
+	t_wi4="Registered for staking: ${R}no${W}"
+	t_wi6="Balance:        ${G}%.2f${W}"
+	t_wi7="Total ROLLs:    ${G}%d${W}"
+	t_wi8="Active ROLLs:   ${G}%d${W}"
 	t_br1="${R}Balance is less than 100 tokens${W}"
 	t_br2="${G}%d${W} ROLLs were bought"
 	t_br3="${G}Enter a ROLL count:${W} "
@@ -42,7 +56,26 @@ address=`jq -r ".balances | keys[0]" <<< $wallet_info`
 if [ "$action" = "client" ]; then
 	./massa-client
 elif [ "$action" = "wallet_info" ]; then
-	./massa-client --cli false wallet_info
+	staking_addresses=`./massa-client --cli true staking_addresses`
+	wallets=`jq -r ".balances | to_entries[]" <<< $wallet_info | tr -d '[:space:]' | sed 's%}{%} {%'`
+	printf_n "$t_wi1"
+	for wallet in $wallets; do
+		w_address=`jq -r ".key" <<< $wallet`
+		w_balance=`jq -r ".value.candidate_ledger_data.balance" <<< $wallet`
+		w_total_rolls=`jq -r ".value.candidate_rolls" <<< $wallet`
+		w_active_rolls=`jq -r ".value.active_rolls" <<< $wallet`
+		printf_n "$t_wi2" $w_address
+		if grep -q "$w_address" <<< "$staking_addresses"; then
+			printf_n "$t_wi3"
+		else
+			printf_n "$t_wi4"
+		fi
+		printf_n "$t_wi6" $w_balance
+		printf_n "$t_wi7" $w_total_rolls
+		printf_n "$t_wi8" $w_active_rolls
+		printf_n
+	done
+	#printf_n t_wi1 `jq -r ".balances[].candidate_ledger_data.balance" <<< $wallet_info`
 elif [ "$action" = "buy_rolls" ]; then
 	balance_float=`jq -r ".balances[].candidate_ledger_data.balance" <<< $wallet_info`
 	balance=`printf "%d" $balance_float 2> /dev/null`
@@ -83,6 +116,11 @@ elif [ "$action" = "cmd_testnet_rewards_program" ]; then
 	read -r discord_id
 	./massa-client --cli true cmd_testnet_rewards_program $address $discord_id
 else
-	printf_n "$t_err"
+	resp=`./massa-client --cli true "$action" 2>&1`
+	if grep -q 'error: Found argument' <<< "$resp"; then
+		printf_n "$t_err"
+	else
+		printf_n "$resp"
+	fi
 fi
 cd
