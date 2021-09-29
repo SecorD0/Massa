ce9bfd8d8026f4d30cb32df7cfd91a95b538752b
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-09-29 19:54:25 +0300

    Performs many functions

--- multi_tool.sh
+++ multi_tool.sh
@@ -10,7 +10,7 @@ while test $# -gt 0; do
 	-h|--help)
 		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 		echo
-		echo -e "${C_LGn}Functionality${RES}: the script installs, updates Massa node, and opens required ports"
+		echo -e "${C_LGn}Functionality${RES}: the script installs, updates a Massa node, and opens required ports"
 		echo
 		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
 		echo
