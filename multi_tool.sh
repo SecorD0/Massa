198a326511b5ec52f1a57eca55dc5467deb6ae60
SecorD0 <vovan6573@gmail.com>
2021-10-07 21:55:55 +0300

    Performs many functions

--- multi_tool.sh
+++ multi_tool.sh
@@ -84,6 +84,7 @@ WantedBy=multi-user.target" > /etc/systemd/system/massad.service
 	sudo systemctl daemon-reload
 	sudo cp $HOME/massa_backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 	sed -i "s%.*# routable_ip *=.*%routable_ip=\"$(wget -qO- eth0.me)\"%" "$HOME/massa/massa-node/config/config.toml"
+	sed -i 's%bootstrap_list *=.*%bootstrap_list = [ [ "62.171.166.224:31245", "8Cf1sQA9VYyUMcDpDRi2TBHQCuMEB7HgMHHdFcsa13m4g6Ee2h",], [ "149.202.86.103:31245", "5GcSNukkKePWpNSjx9STyoEZniJAN4U4EUzdsQyqhuP3WYf6nj",], [ "149.202.89.125:31245", "5wDwi2GYPniGLzpDfKjXJrmHV3p1rLRmm4bQ9TUWNVkpYmd4Zm",], [ "158.69.120.215:31245", "5QbsTjSoKzYc8uBbwPCap392CoMQfZ2jviyq492LZPpijctb9c",], [ "158.69.23.120:31245", "8139kbee951YJdwK99odM7e6V3eW7XShCfX5E2ovG3b9qxqqrq",],]%' "$HOME/massa/massa-node/base_config/config.toml"
 	sudo systemctl restart massad
 	cd $HOME/massa/massa-client/
 	sudo cp $HOME/massa_backup/wallet.dat $HOME/massa/massa-client/wallet.dat
