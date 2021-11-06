68137b11419283d6528f649af83dbe4108e3613d
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-11-07 01:19:43 +0300

    Inserts variables

--- insert_variables.sh
+++ insert_variables.sh
@@ -1,5 +1,6 @@
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_log -v "sudo journalctl -f -n 100 -u massad" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_client -v "cd \$HOME/massa/massa-client/ \&\& ./massa-client; cd" -a
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_node_info -v "cd \$HOME/massa/massa-client/ \&\& ./massa-client get_status; cd" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_cli_client -d
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_wallet_info -v "cd \$HOME/massa/massa-client/ \&\& ./massa-client wallet_info; cd" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_buy_rolls -d
