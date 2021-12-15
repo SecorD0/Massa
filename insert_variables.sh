cb4c7049a642e8c872cbbe5cbce76f86615073f8
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-12-15 15:07:58 +0300

    Inserts variables

--- insert_variables.sh
+++ insert_variables.sh
@@ -1,7 +1,7 @@
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_log -v "sudo journalctl -fn 100 -u massad" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_client -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/cli_client.sh) -l RU -a client" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_cli_client -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/cli_client.sh) -l RU" -a
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_node_info -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/cli_client.sh) -l RU -a node_info" -a
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_node_info -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/cli_client.sh) -l RU -a node_info 2>/dev/null" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_wallet_info -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/cli_client.sh) -l RU -a wallet_info" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_buy_rolls -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/cli_client.sh) -l RU -a buy_rolls" -a
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_peers -d
